c28d00e69082538971762d9a65b5fc1e1d719a19
Vinicius Leoncio <viniciusleoncio87@gmail.com>
2026-04-07 09:58:59 -0300

    add e correção

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,5 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Dashboard from "./pages/Dashboard";
 import Faltas from "./pages/Faltas";
 import Notas from "./pages/Notas";
@@ -7,17 +5,7 @@ import Boletos from "./pages/Boletos";
 import Requerimentos from "./pages/Requerimento";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/faltas" element={<Faltas />} />
-        <Route path="/notas" element={<Notas />} />
-        <Route path="/boletos" element={<Boletos />} />
-        <Route path="/requerimentos" element={<Requerimentos />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Dashboard />;
 }
 
 export default App;
