784fe2fd55bdd88bc04f9ec025264046ea9d1a6d
Vinicius Leoncio <viniciusleoncio87@gmail.com>
2026-06-08 10:37:31 -0300

    add files

--- aulas/aula05/cliente.js
+++ aulas/aula05/cliente.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:5173/produtos";
+const url = "http://localhost:3000/produtos";
 
 
 
--- aulas/aula06/src/pages/Listagem.jsx
+++ aulas/aula06/src/pages/Listagem.jsx
@@ -1,47 +1,53 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router";
-import { listar } from "../services/produtoService";
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router";
+import { listar, remover } from "../services/produtoService";
 
-function Listagem(){
-    const [dados, setDados] = useState ([]);
-    const navigate = useNavigate();
+function Listagem() {
+  const [dados, setDados] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect ( () => {
-        const carregar = async () => {
-            const resposta = await listar();
-            setDados(resposta);
-        };
-        carregar();
-    }, []);
-    return <>
-    <h1>Listagem de Produtos</h1>
-    <button onClick={() => navigate("/produtos/novo")}>Novo</button>
-    <table>
+  const trataRemover = async (produto) => {
+     await remover(produto);
+     setDados(dados.filter((item) => item.id != produto.id));
+  }
+
+  useEffect(() => {
+    const carregar = async () => {
+      const resposta = await listar();
+      setDados(resposta);
+    };
+    carregar();
+  }, []);
+
+  return (
+    <>
+      <h1>Listagem de Produtos</h1>
+      <button onClick={() => navigate("/produtos/novo")}>Novo</button>
+      <table>
         <thead>
-            <tr>
-                <th>Nome do Produto</th>
-                <th>Preço</th>
-                <th>Unidade</th>
-            </tr>
+          <tr>
+            <th>Nome do Produto</th>
+            <th>Preço</th>
+            <th>Unidade</th>
+            <th>Ações</th>
+          </tr>
         </thead>
         <tbody>
-            {dados.map((item) => (
-                <tr key={item.id}>
-                    <td>{item.nome}</td>
-                    <td>{item.preco}</td>
-                    <td>{item.unidade}</td>
-                    <td>
-                        <Link to={` /produto/editar/${item.id}`}>Editar</Link>
-                        <Link to= "/produtos">Remover</Link>
-                    
-                    </td>
-
-                </tr>
-            ))}
+          {dados.map((item) => (
+            <tr key={item.id}>
+              <td>{item.nome}</td>
+              <td>{item.preco}</td>
+              <td>{item.unidade}</td>
+              <td>
+                <Link to={`/produtos/editar/${item.id}`}>Editar</Link>|
+                <Link to="/produtos" onClick={() => trataRemover(item)}>Remover</Link>
+              </td>
+            </tr>
+          ))}
         </tbody>
-    </table>
+      </table>
     </>
+  );
 }
 
 export default Listagem;
